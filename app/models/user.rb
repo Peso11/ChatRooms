43ddcf6d9cd82c9c659625886a0fdf01463f6597
Евгений  Ядевич <answer0333@gmail.com>
2019-04-07 13:25:48 +0300

    Configure basic routes,add message model and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_many :chat_rooms, dependent: :destroy
+  has_many :messages, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def name
+    first_name + '' + last_name
+  end
 end
